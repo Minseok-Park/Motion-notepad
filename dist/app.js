ce239315d69a9263592be106b2be1944adda4853
minseok-Park <pms0905@naver.com>
2021-11-18 15:32:42 +0900

    todo & video & note Component 구현

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,20 @@
+import { NoteComponent } from "./components/page/item/note.js";
 import { ImageComponent } from "./components/page/item/image.js";
 import { PageComponent } from "./components/page/page.js";
+import { TodoComponent } from "./components/page/item/todo.js";
+import { VideoComponent } from "./components/page/item/video.js";
 var App = (function () {
     function App(appRoot) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
         var image = new ImageComponent("Image Title", "https://picsum.photos/600/300");
         image.attachTo(appRoot, "beforeend");
+        var video = new VideoComponent("Video Title", "https://www.youtube.com/embed/cdVREm6xVVk");
+        video.attachTo(appRoot, "beforeend");
+        var note = new NoteComponent("Note Title", "Note Body");
+        note.attachTo(appRoot, "beforeend");
+        var todo = new TodoComponent("Todo Title", "Todo item");
+        todo.attachTo(appRoot, "beforeend");
     }
     return App;
 }());
